3fd73ffc59e5cc3599c82aab38958ce83e396af7
Maksuda Islam Lima <maksudaislam@iut-dhaka.edu>
2021-04-30 20:25:39 +0600

    testing1

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -95,6 +95,9 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
 }
 
 # Password validation
