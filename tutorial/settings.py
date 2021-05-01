8efd347d5ad3b1606a1a4cf4491a24029e4c148b
Maksuda Islam Lima <maksudaislam@iut-dhaka.edu>
2021-05-01 21:58:23 +0600

    debugging

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
-# Heroku: Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
+# Heroku: Update database configuration from $DATABASE_URL. 
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500) 
 DATABASES['default'].update(db_from_env)
